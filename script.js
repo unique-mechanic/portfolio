09bd3b1bbd3d001588f055e0731e417c7629a35a
unique-mechanic <uniquepromechanic@gmail.com>
2023-06-13 05:05:27 +0930

    Changes to be committed: 	new file:   cookbook.html 	modified:   hobbies.html 	modified:   index.html 	new file:   product.html 	new file:   productcontact.html 	new file:   productpage.html 	new file:   recipe1.html 	new file:   recipe2.html 	new file:   script.js 	new file:   stylesheets/productstyle.css 	new file:   stylesheets/style.css 	new file:   stylesheets/style2.css 	new file:   stylesheets/travelstyle.css 	new file:   travelblog.html

---
+++ script.js
@@ -0,0 +1,10 @@
+function addTwoNumbers(){
+    var a = document.getElementById("number1").value;
+         var b = document.getElementById("number2").value;
+           var c = parseInt(a) + parseInt(b); 
+       document.getElementById("screen1").innerHTML=c;
+   }
+
+function createTable(){
+    
+}
